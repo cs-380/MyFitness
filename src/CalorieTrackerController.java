e15c4a85e5f035999ab88c112b9afee0b003c0ec
JacobShank <Shankj@cwu.edu>
2021-11-15 18:00:39 -0800

    Fixed CalorieTrackerController Class

--- src/CalorieTrackerController.java
+++ src/CalorieTrackerController.java
@@ -1,22 +1,16 @@
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-public class CalorieTrackerController {
 
+public class CalorieTrackerController  {
+
+    @FXML
+    private TextField CalorieBox;
+
+    @FXML
+    private Button ConfimationButton;
+
+    
+    
 }
+
